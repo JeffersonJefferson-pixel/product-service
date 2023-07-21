3e009e17b87038857d397970dbd53ebebd6edf2c
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2023-07-21 23:54:06 +0700

    init product service

---
+++ src/main/java/com/yuthika/productservice/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.yuthika.productservice.repository;
+
+import com.yuthika.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+
+
+}
